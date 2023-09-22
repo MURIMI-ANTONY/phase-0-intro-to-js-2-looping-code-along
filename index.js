a9a4a43906419f2899f171158af2cd24a18b72fb
Murimi Antony <amurimi101@gmail.com>
2023-09-22 23:23:30 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+    for (age =30; age <40; age ++){
+        console.log(`I'm ${age} years old. Happy birthday to me!`);
+        debugger;
+    }
+
+const cards = [];
+cards.length=0
+cards.push("Guadalupe", "Ollie", "Aki")
+
+function writeCards(cards,event){
+    let message=[]
+    for (let i=0; i<cards.length; i++){
+        console.log(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`)
+        message.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`)
+        debugger;
+    }
+    return message;
+}
+
+function countDown(x){
+    
+    while(x>=0){
+        console.log(x--)
+    }
+
+}
